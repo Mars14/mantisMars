f75aa568a71752c274eb52553c9bce95422416bb
Martha Jimenez <martha.jimenez@flex.com>
2017-09-06 13:57:43 -0700

    [Studio] Modified screen "Issue Report"

--- client/controllers/app/issuereport.js
+++ client/controllers/app/issuereport.js
@@ -2,4 +2,8 @@ angular.module('app').controller('app_issuereport', app_issuereport);
 function app_issuereport($scope, app) {
     'use strict';
     app.init($scope);
+    
+    $scope.submitReport = function() {
+        //app.action('issuereport', 'submit', this)
+    }
 }
